842a59e438a929bc2be8815161ae4346fcef4d02
lavanya-pg <lavanyapg8@gmail.com>
2021-06-27 18:19:16 +0530

    Intial Commit

--- Emp/EmployeeWage.java
+++ Emp/EmployeeWage.java
@@ -2,9 +2,22 @@ package com.Emp;
 
 public class EmployeeWage
 {
-	public static void main(String[] args) 
-	{
-		System.out.println("Welcome to the Employee Wage Computation");
-	}
+	public static void main(String[] args)
+	   {
+	        Attendance employee = new Attendance();
+	        employee.isPresent();
 
+	    }
+}
+class  Attendance
+{
+	int IS_FULL_TIME = 1;
+	double empCheck = Math.floor(Math.random() * 10) % 2;
+    void isPresent()
+    {
+       if ( empCheck == IS_FULL_TIME )
+    	   System.out.println("Employee is Present ");
+       else
+    	   System.out.println("Employee is Absent ");
+	  }
 }
