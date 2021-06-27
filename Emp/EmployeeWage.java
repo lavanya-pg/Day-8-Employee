34f1ffa0e08578d9efc83e50c7c971a000c56b73
lavanya-pg <lavanyapg8@gmail.com>
2021-06-27 18:23:35 +0530

    ADDED:Calculate Wage

--- Emp/EmployeeWage.java
+++ Emp/EmployeeWage.java
@@ -9,15 +9,27 @@ public class EmployeeWage
 
 	    }
 }
-class  Attendance
+class Attendance extends Wage
 {
-	int IS_FULL_TIME = 1;
-	double empCheck = Math.floor(Math.random() * 10) % 2;
-    void isPresent()
-    {
-       if ( empCheck == IS_FULL_TIME )
-    	   System.out.println("Employee is Present ");
-       else
-    	   System.out.println("Employee is Absent ");
-	  }
+        int IS_FULL_TIME = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        void isPresent()
+        {
+        if ( empCheck == IS_FULL_TIME )
+         {
+            System.out.println("Employee is Present ");
+            System.out.println("Wage is "+dailyWage());
+         }
+        else
+            System.out.println("Employee is Absent ");
+        }
+}
+class Wage
+{
+   final int wagePerHr = 20;
+   final int workHr = 8;
+   int dailyWage()
+   {
+     return wagePerHr * workHr;
+   }
 }
