1a10d20bd716a2910a146372f95e872f15343615
lavanya-pg <lavanyapg8@gmail.com>
2021-06-27 19:08:27 +0530

    ADDED:Monthly wage based on days and hours

--- Emp/EmployeeWage.java
+++ Emp/EmployeeWage.java
@@ -11,38 +11,43 @@ public class EmployeeWage
 }
 class Attendance extends Wage
 {
-    int check;
-    static int days = 0;
-    void isPresent()
-    {
-        int wage = 0;
-        int totalWage = 0;
-        while ( days != 20 )
-        {
-            days++;
-            check = (int)(Math.floor(Math.random() * 10) % 3);
-            switch(check)
-            {
-                case 0:
-                    wage = dailyWage(8);
-                    break;
-                case 1:
-                    wage = dailyWage(4);
-                    break;
-                default:
-                    wage = 0;
-                    break;
-            }
-            totalWage = totalWage + wage;
-        }
-       System.out.println("Total earning of employee for a month is "+totalWage);
+	 int check;
+     static int days = 0;
+     static int workHrs = 0;
+     static int totalWorkHrs = 0;
+     void isPresent()
+   {
+      int wage = 0;
+      int totalWage = 0;
+      while ( days != 20 && totalWorkHrs <= 100 )
+     {
+       days++;
+       System.out.println(days);
+       check = (int)(Math.floor(Math.random() * 10) % 3);
+       switch(check)
+       {
+         case 0:
+             workHrs = 8;
+             break;
+         case 1:
+             workHrs = 4;
+             break;
+         default:
+             workHrs = 0;
+             break;
+       }
+     totalWorkHrs = totalWorkHrs + workHrs;
+     wage = dailyWage(workHrs);
+     totalWage = totalWage + wage;
     }
+     System.out.println("Total earning of employee for a month is "+totalWage);
+  }
 }
 class Wage
 {
      final int wagePerHr = 20;
      int dailyWage(int workHr)
      {
-         return wagePerHr * workHr;
+             return wagePerHr * workHr;
      }
- }
+}
